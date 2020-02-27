14f1652245a01cb6027f544e4cc666ebe79ae173
Twisha Patel <twishapatel09@gmail.com>
2020-02-27 12:46:03 -0800

    Flix: Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by admin on 2/25/20.
+//  Copyright © 2020 Twisha Patel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
